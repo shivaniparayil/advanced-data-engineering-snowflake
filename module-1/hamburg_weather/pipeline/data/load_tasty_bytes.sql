b9103fc1bc1488f4d902517f66b4705b2ad3a76b
shivaniparayil <shvnprl@gmail.com>
2025-10-07 16:57:53 +1100

    Fix missing data

--- module-1/hamburg_weather/pipeline/data/load_tasty_bytes.sql
+++ module-1/hamburg_weather/pipeline/data/load_tasty_bytes.sql
@@ -295,15 +295,6 @@ USE WAREHOUSE demo_build_wh;
 
 -- country table load
 COPY INTO {{env}}_tasty_bytes.raw_pos.country
-(
-   country_id,
-   country,
-   iso_currency,
-   iso_country,
-   city_id,
-   city,
-   city_population
-)
 FROM @{{env}}_tasty_bytes.public.s3load/raw_pos/country/;
 
 
